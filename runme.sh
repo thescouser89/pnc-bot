8c6218c695ef8e36e87f31f65efaf278554ad3d4
Dustin Kut Moy Cheung <dcheung@redhat.com>
2017-03-01 10:05:29 -0500

    ignore some urls

--- runme.sh
+++ runme.sh
@@ -1,3 +1,4 @@
+HUBOT_URL_TITLE_IGNORE_URLS="10.8.172.8:8080|projects.engineering.redhat.com" \
 HUBOT_IRC_SERVER=irc.devel.redhat.com \
 HUBOT_IRC_ROOMS="#dcheung-test" \
 HUBOT_IRC_NICK="pnc-bot55" \
